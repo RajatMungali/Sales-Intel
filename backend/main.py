597de67bf0c23fce3b0174f222cd3101803df043
Rajat <mungalirajat@gmail.com>
2026-06-16 13:50:18 +0530

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://sales-intel-sand.vercel.app/",
+        "https://sales-intel-sand.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
