2f1ed67e7e7392235030e32198824c7eb947337f
Rajat <mungalirajat@gmail.com>
2026-06-16 13:42:44 +0530

    fix backend dependencies

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,11 @@ app = FastAPI(title="Sales Intel API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://sales-intel-sand.vercel.app/",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
